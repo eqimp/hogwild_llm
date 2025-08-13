3794b7a3628d7166d0b667b9c42e7e35128104d8
Vahe1994 <vahe527887@yandex.ru>
2025-08-13 16:01:42 +0200

    Added qwen3 8b support

--- inference_lib/src/kernels/kernel_v21.cuh
+++ inference_lib/src/kernels/kernel_v21.cuh
@@ -394,6 +394,11 @@ cudaError_t hogwild_attention_gpu(scalar_t* out, float scale,
                                                       cudaFuncAttributeMaxDynamicSharedMemorySize, smem));
             hogwild_attention_gpu_kernel21<128, 128, 5><<<grid_dim, block_dim, smem>>>(
                     out, workspace, scale, locations, queries, fragment_lengths, key_fragments, value_fragments, shape);
+        } else if(shape.Hq == shape.Hkv * 4) {
+            CUDA_RETURN_ON_ERROR(cudaFuncSetAttribute(hogwild_attention_gpu_kernel21<128, 128, 8, scalar_t>,
+                                                      cudaFuncAttributeMaxDynamicSharedMemorySize, smem));
+            hogwild_attention_gpu_kernel21<128, 128, 4><<<grid_dim, block_dim, smem>>>(
+                    out, workspace, scale, locations, queries, fragment_lengths, key_fragments, value_fragments, shape);
         } else if(shape.Hq == shape.Hkv * 8) {
             CUDA_RETURN_ON_ERROR(cudaFuncSetAttribute(hogwild_attention_gpu_kernel21<128, 128, 8, scalar_t>,
                                                       cudaFuncAttributeMaxDynamicSharedMemorySize, smem));
